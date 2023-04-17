8fa7fa39b8c8af98978732d9444f3c3367663424
Anderson_Veloso <Andersonhlv@gmail.com>
2023-04-17 10:26:23 -0300

    introdução à exceções em python

---
+++ python_basico/introducao_exception/introducao_excecao.py
@@ -0,0 +1,24 @@
+"""
+Introdução ao try/except
+try -> tentar executar o código
+except -> ocorreu algum erro ao tentar executar
+"""
+numero_str = input(
+    'Vou dobrar o número que vc digitar: '
+)
+
+# Aqui se assemelha a um if - else, em estrutura. 
+try:
+    numero_float = float(numero_str)
+    print('FLOAT:', numero_float)
+    print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+except:
+    print('Isso não é um número')
+
+# O código abaixo geraria um erro, contudo, este erro não seria tradado
+
+# if numero_str.isdigit():
+#     numero_float = float(numero_str)
+#     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+# else:
+#     print('Isso não é um número')
