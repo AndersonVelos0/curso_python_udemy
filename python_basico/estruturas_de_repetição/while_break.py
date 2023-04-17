87b991ae79aab2199e292c538523e6667327c955
Anderson_Veloso <Andersonhlv@gmail.com>
2023-04-17 12:03:23 -0300

    Introduzindo estruturas de repetição em python

---
+++ python_basico/estruturas_de_repetição/while_break.py
@@ -0,0 +1,27 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+
+# condicao = True
+
+# while condicao:
+#     nome = input('Qual seu nome? ')
+#     print(f'Seu nome é {nome}')
+    
+#     if nome == 'sair':
+#         break
+    
+    
+# print('Acabou!')
+
+# Condição para alcançar e sair do loop só 
+condicao = 0
+while condicao <= 10:
+    print(condicao)
+    condicao += 1
+
+print(f'As vezes contadas foram: {condicao}')
+
