738f11ebe60b7c1e37e48f92de39fb71a44777b8
Anderson_Veloso <Andersonhlv@gmail.com>
2023-04-21 19:42:24 -0300

    Adicionando validações ao código gerador de digitos

--- python_basico/gerador_de_digitos_cpf/gerando_segundo_digito.py
+++ python_basico/gerador_de_digitos_cpf/gerando_segundo_digito.py
@@ -21,9 +21,28 @@ contrário disso:
     resultado é o valor da conta
 O segundo dígito do CPF é 0
 """
+# Importando regular expression
+import re
+# Importando o sys 
+import sys
+
+cpf_usuario = input('Digite o seu cpf: ')\
+            # .replace('.', '')\
+            # .replace(' ', '')\
+            # .replace('-', '')  
+# Utilizando regular expression para substituir
+# primeiro é passado o r de expressão regular 
+    # tudo que não for um numero substitua por, significado da expressão abaixo
+Cpf_formatado = re.sub(r'[^0-9]', '', cpf_usuario )
+# Verificando se há digitos iguais
+entrada_e_sequencial = cpf_usuario == cpf_usuario[0] * len(cpf_usuario)
+
+# Caso entre nessa lógica, o código irá parar
+if entrada_e_sequencial:
+    print('Você enviou dados sequenciais!')
+    sys.exit()
 
 
-cpf_usuario = input('Digite o seu cpf: ')
 nove_digitos = cpf_usuario[:9]
 
 contador_regresso_1 = 10
@@ -36,7 +55,7 @@ for digito_1 in nove_digitos:
 
 digito_1 = (resultado_1 *10) % 11
 digito_1 = digito_1 if digito_1 <= 9 else 0
-print(digito_1)
+# print(digito_1)
 
 
 
@@ -56,12 +75,12 @@ for digito_2 in dez_digitos:
 
 digito_2 = (resultado_2 * 10) % 11
 digito_2 = digito_2 if digito_2 <= 9 else 0
-print(digito_2)
+# print(digito_2)
 
 # Gerando o novo cpf 
 
 cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
-print(cpf_gerado_pelo_calculo)
+# print(cpf_gerado_pelo_calculo)
 
 if cpf_usuario == cpf_gerado_pelo_calculo:
     print('CPF enviado pelo usuário é válido!')
