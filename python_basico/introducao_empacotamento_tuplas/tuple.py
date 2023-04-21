64ba7d55165b8f10ae2bacc658d878d1c00e2af4
Anderson_Veloso <Andersonhlv@gmail.com>
2023-04-21 14:21:28 -0300

    Introdução a tupla

---
+++ python_basico/introducao_empacotamento_tuplas/tuple.py
@@ -0,0 +1,19 @@
+"""
+Tipo tupla - Uma lista imutável
+É um pouco mais eficiente que a lista
+"""
+
+# Criando um tipo de dados tuple, sem colchetes
+nomes = ['Maria', 'Helena', 'Luiz']
+# convertendo uma lista para tupla
+nomes = tuple(nomes)
+# convertendo uma tupla em lista
+nomes = list(nomes)
+print(nomes[0])
+print(nomes)
+
+
+# O erro seguinte será gerado caso tentemos alterar algum valor da tupla:
+# TypeError: 'tuple' object does not support item assignment
+# nomes[1] = 'Outro'
+
