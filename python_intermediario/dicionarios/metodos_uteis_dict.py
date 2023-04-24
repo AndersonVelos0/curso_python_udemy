8cc17ef1108c7f56f692f78e5f81ac79912b52cb
Anderson_Veloso <Andersonhlv@gmail.com>
2023-04-24 15:31:02 -0300

    Métodos uteis para trabalhar com dicionarios em python

---
+++ python_intermediario/dicionarios/metodos_uteis_dict.py
@@ -0,0 +1,37 @@
+# Métodos úteis dos dicionários em Python
+# len - quantas chaves
+# keys - iterável com as chaves
+# values - iterável com os valores
+# items - iterável com chaves e valores
+# setdefault - adiciona valor se a chave não existe
+# copy - retorna uma cópia rasa (shallow copy)
+# get - obtém uma chave
+# pop - Apaga um item com a chave especificada (del)
+# popitem - Apaga o último item adicionado
+# update - Atualiza um dicionário com outro
+pessoa = {
+    'nome': 'Luiz Otávio',
+    'sobrenome': 'Miranda',
+    # 'idade': 900,
+}
+# Função len que chama o método dunder len __len__
+print(len(pessoa))
+
+# Função para retornar as chaves do dict 
+# Você pode retornar no tipo que preferri
+print(pessoa.keys())
+print(list(pessoa.keys()))
+
+#Retornando os valores
+print(list(pessoa.values()))
+
+# Retornando as chaves e o valor 
+print(list(pessoa.items()))
+
+# Caso uma chave não exista e você queira definir um valor padrão para defini-la, utilizamos setdefault 
+pessoa.setdefault('idade', 20)
+print(pessoa['idade'])
+
+# Esse for do enumerate faz a mesma coisa que a função items
+for chave, valor in pessoa.items():
+    print(chave, valor)
