d00fb144b4be6d83002e00288f39ae015b94fddf
Anderson_Veloso <Andersonhlv@gmail.com>
2023-04-24 14:18:51 -0300

    Introducao ao uso dos dicionarios em python

---
+++ python_intermediario/dicionarios/intro_dicionarios.py
@@ -0,0 +1,44 @@
+# Dicionários em Python (tipo dict)
+# Dicionários são estruturas de dados do tipo
+# par de "chave" e "valor".
+# Chaves podem ser consideradas como o "índice"
+# que vimos na lista e podem ser de tipos imutáveis
+# como: str, int, float, bool, tuple, etc.
+# O valor pode ser de qualquer tipo, incluindo outro
+# dicionário.
+# Usamos as chaves - {} - ou a classe dict para criar
+# dicionários.
+# Imutáveis: str, int, float, bool, tuple
+# Mutável: dict, list
+# pessoa = {
+#     'nome': 'Luiz Otávio',
+#     'sobrenome': 'Miranda',
+#     'idade': 18,
+#     'altura': 1.8,
+#     'endereços': [
+#         {'rua': 'tal tal', 'número': 123},
+#         {'rua': 'outra rua', 'número': 321},
+#     ]
+# }
+# pessoa = dict(nome='Luiz Otávio', sobrenome='Miranda')
+
+pessoa = {
+    # devemos indicar os indices e os dois pontos valem como um igual = 
+    'nome': 'Anderson Veloso',
+    'sobrenome': 'Luna',
+    'idade': 18,
+    'altura': 1.8,
+    # Dentro do dicionario de endereços, tem uma lista de endereços
+    'endereços': [
+        {'rua': 'tal tal', 'número': 123},
+        {'rua': 'outra rua', 'número': 321},
+    ],
+    
+}
+# um dicionario não é iteravel, mas o python tem mecanismos de iteração  
+print(pessoa['nome'])
+print(pessoa['sobrenome'])
+print()
+
+for chave in pessoa:
+    print(chave, pessoa[chave])
