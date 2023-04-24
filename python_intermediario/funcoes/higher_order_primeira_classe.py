7874cefde5de88a9f65ea38c9cfe3c02cfc02e6b
Anderson_Veloso <Andersonhlv@gmail.com>
2023-04-24 12:17:36 -0300

    Higher functions e a passagem de outras funções como parametros de uma função

---
+++ python_intermediario/funcoes/higher_order_primeira_classe.py
@@ -0,0 +1,33 @@
+"""
+Higher Order Functions
+Funções de primeira classe
+"""
+
+# As funções em python podem ser tratadas como qualquer tipo de dados
+
+def saudacao(msg, nome):
+    return f'{msg}, {nome}!'
+
+# A função executa irá executar a função saudacao e o resto está passando parametros desconhecidos no final, caso queira adicionar
+def executa(funcao, *args):
+    # Há o desempacotamento abaixo
+    return funcao(*args)
+
+# É possivel passar uma função como parametro de outra funçao
+print(
+    executa(saudacao, 'Bom dia', 'Luiz')
+)
+print(
+    executa(saudacao, 'Boa noite', 'Maria')
+)
+
+
+# Academicamente, os termos Higher Order Functions e First-Class Functions têm significados diferentes.
+
+# Higher Order Functions - Funções que podem receber e/ou retornar outras funções
+
+# First-Class Functions - Funções que são tratadas como outros tipos de dados comuns (strings, inteiros, etc...)
+
+# Não faria muita diferença no seu código, mas penso que deveria lhe informar isso.
+
+# Observação: esses termos podem ser diferentes e ainda refletir o mesmo significado.
