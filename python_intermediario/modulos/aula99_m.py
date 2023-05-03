1b8edaa33b166ad006e5324109d70f06f0f294de
Anderson Velos0 <andersonhlv@gmail.com>
2023-05-03 16:12:17 -0300

    Descommit

--- python_intermediario/modulos/aula99_m.py
+++ python_intermediario/modulos/aula99_m.py
@@ -1,17 +1,17 @@
-# from sys import path
+from sys import path
 
-# # Formas de importar o módulo 
-# import testandoModularizacao.modulo
-# from testandoModularizacao import modulo
-# from testandoModularizacao.modulo import *
+# Formas de importar o módulo 
+import testandoModularizacao.modulo
+from testandoModularizacao import modulo
+from testandoModularizacao.modulo import *
 
-# # from aula99_package.modulo import soma_do_modulo
+# from aula99_package.modulo import soma_do_modulo
 
-# # print(*path, sep='\n')
-# print(soma_do_modulo(1, 2))
-# print(testandoModularizacao.modulo.soma_do_modulo(1, 2))
-# print(modulo.soma_do_modulo(1, 2))
-# print(variavel)
-# print(nova_variavel)
+# print(*path, sep='\n')
+print(soma_do_modulo(1, 2))
+print(testandoModularizacao.modulo.soma_do_modulo(1, 2))
+print(modulo.soma_do_modulo(1, 2))
+print(variavel)
+print(nova_variavel)
 
-import testandoModularizacao
+# import testandoModularizacao
