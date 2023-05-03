0a96e7c38e8490fa2524acc9998f93d9c9da4ffc
Anderson Velos0 <andersonhlv@gmail.com>
2023-05-03 16:06:25 -0300

    Utilizando o __init__ para permitir o uso dos outros modulos

---
+++ python_intermediario/modulos/testandoModularizacao/__init__.py
@@ -0,0 +1,10 @@
+
+# Módulo de inicialização, ele é executado assim que o módulo é executado
+# Você pode utilizar para " enganar o python" fazendo o python acreditar que o init é um modulo
+
+# Exemplo de uso 
+
+# Tudo que foi importado no init, você poderá utilizar nos módulos, neste caso o import all seria um recurso 
+# Você permite e abre caminho
+from testandoModularizacao.modulo import *
+from testandoModularizacao.modulo_b import *
